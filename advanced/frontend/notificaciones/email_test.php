c7dbc0fe4e6d251cabf3aa08224c5416a90e6585
VAIO\David <dasensag@gmail.com>
2017-10-23 21:37:10 -0700

    Notificaciones de que se acaba el tiempo

---
+++ advanced/frontend/notificaciones/email_test.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+require_once("email.php");
+
+final class EmailTest extends TestCase
+{
+    public function send_email_test(): void
+    {
+        $this->assertIsTrue(send_email());
+    }
+}
